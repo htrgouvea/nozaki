c36e4db4070ba18a6cef4398be8a45bed63acf92
GouveaHeitor <hi@heitorgouvea.me>
2020-07-28 13:46:02 -0300

    adjust package code structure

--- lib/Engine/Fuzzer.pm
+++ lib/Engine/Fuzzer.pm
@@ -1,41 +1,41 @@
-package Engine::Fuzzer;
+package Engine::Fuzzer {
+    use strict;
+    use warnings;
+    use HTTP::Request;
+    use LWP::UserAgent;
 
-use strict;
-use warnings;
-use HTTP::Request;
-use LWP::UserAgent;
+    sub new {
+        my ($self, $method, $endpoint, $timeout, $delay, $agent, $return) = @_;
 
-sub new {
-    my ($self, $method, $endpoint, $timeout, $delay, $agent, $return) = @_;
+        my $ua = LWP::UserAgent -> new (
+            timeout => $timeout,
+            agent => $agent
+        );
 
-    my $ua = LWP::UserAgent -> new (
-        timeout => $timeout,
-        agent => $agent
-    );
+        my @verbs = split(",", $method);
 
-    my @verbs = split(",", $method);
+        foreach my $verb (@verbs) {
+            my $request  = new HTTP::Request($verb, $endpoint);
+            my $response = $ua -> request($request);
+            my $code     = $response -> code();
+            my $message  = $response -> message();
+            my $length   = $response -> content_length() || "null";
 
-    foreach my $verb (@verbs) {
-        my $request  = new HTTP::Request($verb, $endpoint);
-        my $response = $ua -> request($request);
-        my $code     = $response -> code();
-        my $message  = $response -> message();
-        my $length   = $response -> content_length() || "null";
+            if ($return) { # Yeah, I know, i need refact that shit
+                if ($code == $return) {
+                    print "[$code] | $endpoint \t [$verb] - $message | Length: $length\n";
+                }
+            }
 
-        if ($return) { # Yeah, I know, i need refact that shit
-            if ($code == $return) {
+            else {
                 print "[$code] | $endpoint \t [$verb] - $message | Length: $length\n";
             }
+            
+            sleep($delay);
         }
 
-        else {
-            print "[$code] | $endpoint \t [$verb] - $message | Length: $length\n";
-        }
-        
-        sleep($delay);
+        return 1;
     }
-
-    return 1;
 }
 
 1;
--- lib/Functions/Helper.pm
+++ lib/Functions/Helper.pm
@@ -1,25 +1,25 @@
-package Functions::Helper;
+package Functions::Helper {
+	use strict;
+	use warnings;
 
-use strict;
-use warnings;
-
-sub new {
-    print "
-        \rNozaki v0.0.7
-		\rCore Commands
-		\r==============
-		\r\tCommand       Description
-		\r\t-------       -----------
-		\r\t--url         Define a target
-		\r\t--wordlist    Define wordlist of paths
-		\r\t--method      Define methods HTTP to use during fuzzing, separeted by \",\"
-		\r\t--delay       Define a seconds of delay between requests
-		\r\t--timeout     Define the timeout 
-		\r\t--agent       Define a custom User Agent
-		\r\t--return      Set a filter based on HTTP Code Response
-		\r\t--help        See this screen\n\n";
-    
-    return 1;
+	sub new {
+		print "
+			\rNozaki v0.0.7
+			\rCore Commands
+			\r==============
+			\r\tCommand       Description
+			\r\t-------       -----------
+			\r\t--url         Define a target
+			\r\t--wordlist    Define wordlist of paths
+			\r\t--method      Define methods HTTP to use during fuzzing, separeted by \",\"
+			\r\t--delay       Define a seconds of delay between requests
+			\r\t--timeout     Define the timeout 
+			\r\t--agent       Define a custom User Agent
+			\r\t--return      Set a filter based on HTTP Code Response
+			\r\t--help        See this screen\n\n";
+		
+		return 1;
+	}
 }
 
 1;
