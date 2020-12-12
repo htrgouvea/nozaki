8c58d3091a3e0fd85c03ee3967820d5fd1925157
Lucas V. Araujo <lucas.vieira.ar@disroot.org>
2020-12-12 16:47:29 -0300

    replaced threads with Parallel::ForkManager

--- nozaki.pl
+++ nozaki.pl
@@ -3,60 +3,39 @@
 use JSON;
 use 5.018;
 use strict;
-use threads;
 use warnings;
-use Getopt::Long;
 use lib "./lib/";
-use Thread::Queue;
 use Engine::Fuzzer;
-use threads::shared;
 use Functions::Utils;
 use Functions::Helper;
-
-my $endpoint_queue = Thread::Queue->new();
+use Parallel::ForkManager;
+use Getopt::Long qw(:config no_ignore_case);
 
 sub fuzzer_thread
 {
-    my ($target, $methods,
-        $agent, $headers,
-        $accept, $timeout,
-        $return, $payload,
-        $json, $delay) = @_;
+    my ($endpoint, $methods,$agent, $headers, $accept,
+        $timeout, $return, $payload, $json, $delay) = @_;
     
     my $fuzzer = Engine::Fuzzer->new(
-        useragent => $agent,
-        timeout => $timeout,
-        headers => { %$headers },
+            useragent => $agent,
+            timeout => $timeout,
+            headers => $headers,
     );
     
     my @verbs = split /,/, $methods;
-
-    while (defined(my $res = $endpoint_queue->dequeue()))
+    for my $verb (@verbs)
     {
-        my $endpoint = url_join($target, $res);
-        for my $verb (@verbs)
-        {
-            my $result = $fuzzer->fuzz($endpoint, $verb, $payload, $accept);
+        my $result = $fuzzer->fuzz($endpoint, $verb, $payload, $accept);
 
-            next if $return && $result->{Code} != $return;
-            
-            my $printable;
-            
-            if ($json)
-            {
-                $printable = encode_json($result);
-            }
-            else
-            {
-                $printable = sprintf(
-                    "[%d] URL: %s | Method: %s | Response: %s | Length: %s",
-                    $result->{Code}, $result->{URL}, $result->{Method},
-                    $result->{Response}, $result->{Length}
-                );
-            }
-            print $printable . "\n";
-            sleep($delay);
-        }
+        next if $return && $result->{Code} != $return;
+        
+        my $printable = $json ? encode_json($result) : sprintf(
+            "[%d] URL: %s | Method: %s | Response: %s | Length: %s",
+            $result->{Code}, $result->{URL}, $result->{Method},
+            $result->{Response}, $result->{Length}
+        );
+        print $printable . "\n";
+        sleep($delay);
     }
 }
 
@@ -65,14 +44,14 @@ sub main
     my ($target, $return, $payload);
     my %headers;
     my $agent    = "Nozaki CLI / 0.1.1";
-    my $delay    = 0.3;
+    my $delay    = 0;
     my $timeout  = 10;
     my $wordlist = "wordlists/default.txt";
     my $methods  = "GET,POST,PUT,DELETE,HEAD,OPTIONS,CONNECT,TRACE,PATCH,SUBSCRIBE,MOVE,REPORT,UNLOCK,%s%s%s%s"
                     . "PURGE,POLL,NOTIFY,SEARCH,1337,CATS,*,DATA,HEADERS,PRIORITY,RST_STREAM,SETTINGS,PUSH_PROMISE"
                     . "PING,GOAWAY,WINDOW_UPDATE,CONTINUATION";
-    #use only 5 threads by default
-    my $tasks    = 5; 
+    #use only 10 threads by default
+    my $tasks    = 10; 
     my $json     = 0;
     my $accept;
 
@@ -90,33 +69,36 @@ sub main
         "H|header=s%"  => \%headers,
         "T|tasks=i"    => \$tasks,
     ) or die ( return Functions::Helper -> new() );
+
     return Functions::Helper->new() unless $target && $wordlist;
     
     open (my $file, "<", $wordlist) || die "$0: Can't open $wordlist";
-
+    my @wordlist;
     while (<$file>)
     {
         chomp;
-        $endpoint_queue->enqueue($_);
+        push @wordlist, $_;
     }
-
-    $endpoint_queue->end();
     close ($file);
 
-    #threads
-    async {
-        foreach (1 .. $tasks)
-        {
-            threads->create("fuzzer_thread", $target, $methods, $agent,
-                \%headers, $accept, $timeout, $return, $payload, $json, $delay
-            );
-        }
-    };
+    my $threadmgr = Parallel::ForkManager->new($tasks);
+    $threadmgr->set_waitpid_blocking_sleep(0);
+    THREADS:
+    for (@wordlist)
+    {
+        my $endpoint = url_join($target, $_);
+        $threadmgr->start() and next THREADS;
+        
+        fuzzer_thread($endpoint, $methods, $agent, \%headers, $accept,
+            $timeout, $return, $payload, $json, $delay);
+        
+        $threadmgr->finish();
+    }
+    $threadmgr->wait_all_children;
 
-    while (threads->list(threads::running) > 0) {};
-    map { $_->join() } threads->list(threads::all);
-    print "Done.\n";
-    return 0;    
+    print "Finished.\n";
+    
+    return 0;
 }
 
 exit main();
