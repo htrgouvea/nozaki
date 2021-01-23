9675ef80a3820cb0c2eda36a9ce6e54dae6dce79
Lucas V. Araujo <lucas.vieira.ar@disroot.org>
2021-01-22 23:26:06 -0300

    changes on Helper.pm

--- lib/Functions/Helper.pm
+++ lib/Functions/Helper.pm
@@ -12,16 +12,16 @@ package Functions::Helper {
             \r\t-A, --accept      Define a custom 'Accept' header
             \r\t-T, --tasks       The number of threads to run in parallel
             \r\t-H, --header      Define a custom header (header=value)
-			\r\t-m, --method      Define methods HTTP to use during fuzzing, separeted by \",\"
+			\r\t-m, --method      Define HTTP methods to use during fuzzing, separeted by \",\"
 			\r\t-u, --url         Define a target
 			\r\t-w, --wordlist    Define wordlist of paths
-			\r\t-d, --delay       Define a seconds of delay between requests
+			\r\t-d, --delay       Define seconds of delay between requests
 			\r\t-a, --agent       Define a custom User Agent
 			\r\t-r, --return      Set a filter based on HTTP Response Code
-			\r\t-e, --exclude     Exclude a specif result based on HTTP Response Code
+			\r\t-e, --exclude     Exclude a specific result based on HTTP Response Code
 			\r\t-t, --timeout     Define the timeout, default is 10s
 			\r\t-p, --payload     Send a custom data
-			\r\t-j, --json        Define the output in JSON format
+			\r\t-j, --json        Display the results in JSON format
 			\r\t-h, --help        See this screen\n\n";
 					
 		return 1;
