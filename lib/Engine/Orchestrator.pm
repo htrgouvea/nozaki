71dc8fb07ccbd6dbdf0feb3ca76d31c203e51b23
Lucas V. Araujo <lucas.vieira.ar@disroot.org>
2021-05-21 14:04:41 -0300

    improved option handling and fixed some (possible) issues

--- lib/Engine/Orchestrator.pm
+++ lib/Engine/Orchestrator.pm
@@ -24,52 +24,28 @@ package Engine::Orchestrator  {
     }
 
     sub run_fuzzer {
-        my ($self, $target, $args) = @_;
-        my ($return, $payload, %headers, $accept, $json, $exclude, $skipssl);
-        my $delay    = 0;
-        my $timeout  = 10;
-        my $agent    = "Nozaki / 0.2.5";
-        my $wordlist = "wordlists/default.txt";
-        my $methods  = "GET,POST,PUT,DELETE,HEAD,OPTIONS,TRACE,PATCH,PUSH";
-        my $tasks    = 10;
-        my $length;
-
-        Getopt::Long::GetOptionsFromArray (
-            $args,
-            "A|accept=s"   => \$accept,
-            "w|wordlist=s" => \$wordlist,
-            "m|method=s"   => \$methods,
-            "d|delay=i"    => \$delay,
-            "t|timeout=i"  => \$timeout,
-            "a|agent=s"    => \$agent,
-            "r|return=s"   => \$return,
-            "p|payload=s"  => \$payload,
-            "j|json"       => \$json,
-            "H|header=s%"  => \%headers,
-            "T|tasks=i"    => \$tasks,
-            "e|exclude=s"  => \$exclude,
-            "S|skip-ssl"   => \$skipssl,
-            "l|length=s"   => \$length,
-        );
+        my ($self, %options) = @_;
 
         my @current = map {
             open(my $fh, "<$_") || die "$0: Can't open $_: $!";
             $fh
-        } glob($wordlist);
+        } glob($options{wordlist});
         
         $wordlist_queue = Thread::Queue -> new();
 
-        fill_queue(\@current, 10 * $tasks);
+        fill_queue(\@current, 10 * $options{tasks});
 
-        for (1 .. $tasks) {
+        for (1 .. $options{tasks}) {
             Engine::FuzzerThread -> new (
-                $wordlist_queue, $target, $methods, $agent, \%headers, $accept, $timeout,
-                $return, $payload, $json, $delay, $exclude, $skipssl, $length
+                $wordlist_queue, $options{target}, $options{method}, $options{agent},
+                $options{headers}, $options{accept}, $options{timeout}, $options{return},
+                $options{payload}, $options{json}, $options{delay}, $options{exclude},
+                $options{skipssl}, $options{length}
             );
         }
 
         while (threads -> list(threads::running) > 0) {
-            fill_queue(\@current, $tasks);
+            fill_queue(\@current, $options{tasks});
         }
 
         map { $_ -> join() } threads -> list(threads::all);
