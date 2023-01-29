2c85381705151eb5b3ff376169186c5a09567065
htrgouvea <hi@heitorgouvea.me>
2023-01-29 20:04:58 -0300

    drafting plugins

---
+++ plugins/encode/base64.pm
@@ -0,0 +1,12 @@
+package Plugins::Encode::Base64 {
+    use strict;
+    use warnings;
+
+    sub new {
+        my ($self, @params) = @_;
+        
+        return 1;
+    }
+}
+
+1;
---
+++ plugins/seeds/swagger.pm
@@ -0,0 +1,12 @@
+package Plugins::Seeds::Swagger {
+    use strict;
+    use warnings;
+
+    sub new {
+        my ($self, @params) = @_;
+        
+        return 1;
+    }
+}
+
+1;
