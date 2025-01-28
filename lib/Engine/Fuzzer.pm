830e160ee13fcf0b9398ec7932f4c683d25e5535
priv <140729444+scriptprivate@users.noreply.github.com>
2025-01-28 09:06:57 -0300

    fix: resolve PBP violations (#51)
    * fix: resolve PBP violations for boolean precedence and grep
    
    * fix: resolve PBP violations in subroutines and file handling
    
    * fix: add explicit returns to subroutines per PBP

--- lib/Engine/Fuzzer.pm
+++ lib/Engine/Fuzzer.pm
@@ -7,39 +7,41 @@ package Engine::Fuzzer {
     sub new {
         my ($self, $timeout, $headers, $skipssl, $proxy) = @_;
 
-        my $userAgent = Mojo::UserAgent -> new() -> request_timeout($timeout) -> insecure($skipssl);
+        my $userAgent = Mojo::UserAgent->new()->request_timeout($timeout)->insecure($skipssl);
 
         if ($proxy) {
-            $userAgent -> proxy -> http($proxy);
-            $userAgent -> proxy -> https($proxy);
+            $userAgent->proxy->http($proxy);
+            $userAgent->proxy->https($proxy);
         }
 
-        bless {
+        my $instance = bless {
             useragent => $userAgent,
             headers   => $headers
         }, $self;
+
+        return $instance;
     }
 
     sub request {
         my ($self, $method, $agent, $endpoint, $payload, $accept) = @_;
 
-        my $request = $self -> {useragent} -> build_tx (
+        my $request = $self->{useragent}->build_tx(
             $method => $endpoint => {
                 "User-Agent" => $agent,
-                %{$self -> {headers}}
+                %{$self->{headers}}
             } => $payload || ""
         );
 
         try {
-            my $response  = $self -> {useragent} -> start($request) -> result();
+            my $response = $self->{useragent}->start($request)->result();
 
             my $result = {
                 "Method"   => $method,
                 "URL"      => $endpoint,
-                "Code"     => $response -> code(),
-                "Response" => $response -> message(),
-                "Content"  => $response -> body(),
-                "Length"   => $response -> headers() -> content_length() || "0"
+                "Code"     => $response->code(),
+                "Response" => $response->message(),
+                "Content"  => $response->body(),
+                "Length"   => $response->headers()->content_length() || "0"
             };
 
             return $result;
@@ -47,7 +49,9 @@ package Engine::Fuzzer {
 
         catch {
             return 0;
-        }
+        };
+        
+        return;
     }
 }
 
--- lib/Engine/FuzzerThread.pm
+++ lib/Engine/FuzzerThread.pm
@@ -4,70 +4,70 @@ package Engine::FuzzerThread {
     use threads;
     use warnings;
     use Engine::Fuzzer;
-
+   
     sub new {
         my (
             $self, $queue, $target, $methods, $agent, $headers, $accept,
             $timeout, $return, $payload, $json, $delay, $exclude, $skipssl,
             $length, $content, $proxy
         ) = @_;
-
+       
         my @verbs         = split (/,/, $methods);
         my @valid_codes   = split /,/, $return || "";
         my @invalid_codes = split /,/, $exclude || "";
-
+       
         my $fuzzer = Engine::Fuzzer -> new($timeout, $headers, $skipssl, $proxy);
         my $format = JSON -> new() -> allow_nonref() -> pretty();
-
+       
         my $cmp;
-
+       
         if ($length) {
             ($cmp, $length) = $length =~ /([>=<]{0,2})(\d+)/;
-
             $cmp = sub { $_[0] >= $length } if ($cmp eq ">=");
             $cmp = sub { $_[0] <= $length } if ($cmp eq "<=");
             $cmp = sub { $_[0] != $length } if ($cmp eq "<>");
             $cmp = sub { $_[0]  > $length } if ($cmp eq  ">");
             $cmp = sub { $_[0]  < $length } if ($cmp eq  "<");
-            $cmp = sub { $_[0] == $length } if (!$cmp or $cmp eq "=");
+            $cmp = sub { $_[0] == $length } if (defined($cmp) || $cmp eq "="); 
         }
-
+       
         async {
             while (defined(my $resource = $queue -> dequeue())) {
-                my $endpoint = $target . $resource;
+                my $endpoint = $target . $resource;               
                 my $found = 0;
-
+               
                 for my $verb (@verbs) {
                     my $result = $fuzzer -> request($verb, $agent, $endpoint, $payload, $accept);
-
+                   
                     unless ($result) {
                         next;
                     }
-
+                   
                     my $status = $result -> {Code};
-
-                    if (grep(/^$status$/, @invalid_codes) || ($return && !grep(/^$status$/, @valid_codes))) {
+                   
+                    if (scalar(grep { $_ eq $status } @invalid_codes) || ($return && !scalar(grep { $_ eq $status } @valid_codes))) {
                         next;
                     }
-                    
+                   
                     if ($length && !($cmp -> ($result -> {Length}))) {
                         next;
                     }
-
+                   
                     my $message = $json ? $format -> encode($result) : sprintf(
                         "Code: %d | URL: %s | Method: %s | Response: %s | Length: %s",
                         $status, $result -> {URL}, $result -> {Method},
                         $result -> {Response} || "?", $result -> {Length}
                     );
-
+                   
                     print $message, "\n" if !$content || $result -> {Content} =~ m/$content/;
-
+                   
                     sleep($delay);
+                   
                     $found = 1;
                 }
             }
         };
-
+        
         return 1;
     }
 }
