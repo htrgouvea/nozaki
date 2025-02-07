5d1980269681d29816914740c423c9cf77778e1d
priv <140729444+scriptprivate@users.noreply.github.com>
2025-02-07 18:30:29 -0300

    feat: add --randomize-agent and --custom-agent

--- nozaki.pl
+++ nozaki.pl
@@ -16,35 +16,39 @@ sub main {
     my ($workflow, @targets);
 
     my %options = (
-        accept   => "*/*",
-        wordlist => "wordlists/default.txt",
-        method   => "GET,POST,PUT,DELETE,HEAD,OPTIONS,PATCH,PUSH",
-        headers  => {},
-        timeout  => 10,
-        agent    => "Nozaki / 0.2.9",
-        tasks    => 10,
-        delay    => 0
+        accept            => "*/*",
+        wordlist          => "wordlists/default.txt",
+        method            => "GET,POST,PUT,DELETE,HEAD,OPTIONS,PATCH,PUSH",
+        headers           => {},
+        timeout           => 10,
+        agent             => "Nozaki / 0.2.9",
+        tasks             => 10,
+        delay             => 0,
+        randomize_agent   => 0,
+        custom_agents     => undef
     );
 
     Getopt::Long::GetOptions (
-        "A|accept=s"   => \$options{accept},
-        "a|agent=s"    => \$options{agent},
-        "c|content=s"  => \$options{content},
-        "d|delay=i"    => \$options{delay},
-        "e|exclude=s"  => \$options{exclude},
-        "H|header=s%"  => \$options{headers},
-        "w|wordlist=s" => \$options{wordlist},
-        "W|workflow=s" => \$workflow,
-        "m|method=s"   => \$options{method},
-        "r|return=s"   => \$options{return},
-        "p|payload=s"  => \$options{payload},
-        "j|json"       => \$options{json},
-        "S|skip-ssl"   => \$options{skipssl},
-        "T|tasks=i"    => \$options{tasks},
-        "t|timeout=i"  => \$options{timeout},
-        "u|url=s@"     => \@targets,
-        "l|length=s"   => \$options{length},
-        "P|proxy=s"    => \$options{proxy}
+        "A|accept=s"   		  => \$options{accept},
+        "a|agent=s"    		  => \$options{agent},
+        "c|content=s"  		  => \$options{content},
+        "d|delay=i"    		  => \$options{delay},
+        "e|exclude=s"  		  => \$options{exclude},
+        "H|header=s%"  		  => \$options{headers},
+        "w|wordlist=s" 		  => \$options{wordlist},
+        "W|workflow=s"        => \$workflow,
+        "m|method=s"          => \$options{method},
+        "r|return=s"          => \$options{return},
+        "p|payload=s"         => \$options{payload},
+        "j|json"              => \$options{json},
+        "S|skip-ssl"          => \$options{skipssl},
+        "T|tasks=i"           => \$options{tasks},
+        "t|timeout=i"         => \$options{timeout},
+        "u|url=s@"            => \@targets,
+        "l|length=s"          => \$options{length},
+        "P|proxy=s"           => \$options{proxy},
+        "R|randomize-agent=s" => \$options{randomize_agent},
+        "C|custom-agent=s"    => \$options{custom_agents}
     );
 
     return Functions::Helper -> new() unless @targets;
