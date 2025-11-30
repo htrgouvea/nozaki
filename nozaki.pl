f9d9c5cd2b12fc47cbd391feeaf43161b8369538
htrgouvea <10741284+htrgouvea@users.noreply.github.com>
2025-11-30 20:18:07 -0300

    Refactoring/fixing linter warnings (#69)
    * 🚑 fix: update Engine::Fuzzer version to '0.0.3' and refine request method parameters
    
    * 🚑 fix: fix string comparison issues in Engine::Fuzzer and Engine::FuzzerThread modules

--- nozaki.pl
+++ nozaki.pl
@@ -5,7 +5,7 @@ use strict;
 use threads;
 use warnings;
 use Thread::Queue;
-use Find::Lib "./lib";
+use Find::Lib './lib';
 use Functions::Helper;
 use Functions::Parser;
 use Engine::Orchestrator;
@@ -17,35 +17,35 @@ sub main {
     my ($workflow, @targets);
 
     my %options = (
-        accept   => "*/*",
-        wordlist => "wordlists/default.txt",
-        method   => "GET,POST,PUT,DELETE,HEAD,OPTIONS,PATCH,PUSH",
+        accept   => '*/*',
+        wordlist => 'wordlists/default.txt',
+        method   => 'GET,POST,PUT,DELETE,HEAD,OPTIONS,PATCH,PUSH',
         headers  => {},
         timeout  => 10,
-        agent    => "Nozaki / 0.3.1",
+        agent    => 'Nozaki / 0.3.1',
         tasks    => 35,
         delay    => 0
     );
 
     Getopt::Long::GetOptions (
-        "A|accept=s"   => \$options{accept},
-        "a|agent=s"    => \$options{agent},
-        "c|content=s"  => \$options{content},
-        "d|delay=i"    => \$options{delay},
-        "e|exclude=s"  => \$options{exclude},
-        "H|header=s%"  => \$options{headers},
-        "w|wordlist=s" => \$options{wordlist},
-        "W|workflow=s" => \$workflow,
-        "m|method=s"   => \$options{method},
-        "r|return=s"   => \$options{return},
-        "p|payload=s"  => \$options{payload},
-        "j|json"       => \$options{json},
-        "S|skip-ssl"   => \$options{skipssl},
-        "T|tasks=i"    => \$options{tasks},
-        "t|timeout=i"  => \$options{timeout},
-        "u|url=s@"     => \@targets,
-        "l|length=s"   => \$options{length},
-        "P|proxy=s"    => \$options{proxy}
+        'A|accept=s'   => \$options{accept},
+        'a|agent=s'    => \$options{agent},
+        'c|content=s'  => \$options{content},
+        'd|delay=i'    => \$options{delay},
+        'e|exclude=s'  => \$options{exclude},
+        'H|header=s%'  => \$options{headers},
+        'w|wordlist=s' => \$options{wordlist},
+        'W|workflow=s' => \$workflow,
+        'm|method=s'   => \$options{method},
+        'r|return=s'   => \$options{return},
+        'p|payload=s'  => \$options{payload},
+        'j|json'       => \$options{json},
+        'S|skip-ssl'   => \$options{skipssl},
+        'T|tasks=i'    => \$options{tasks},
+        't|timeout=i'  => \$options{timeout},
+        'u|url=s@'     => \@targets,
+        'l|length=s'   => \$options{length},
+        'P|proxy=s'    => \$options{proxy}
     );
 
     if (!@targets) {
